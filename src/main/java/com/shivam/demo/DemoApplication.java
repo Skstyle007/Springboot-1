b0f77f7414547e2c0f8692db6214286d23a66032
Skstyle007 <shivam001stm@gmail.com>
2026-07-13 11:25:32 +0530

    first commit create student object

---
+++ src/main/java/com/shivam/demo/DemoApplication.java
@@ -0,0 +1,17 @@
+package com.shivam.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+		Student student = new Student("Shivam",20,100);
+		System.out.println(student.getAge());
+		System.out.println(student.getName());
+	}
+
+
+}
