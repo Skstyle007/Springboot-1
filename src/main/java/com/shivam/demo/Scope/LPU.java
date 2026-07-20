e2384b1c3d4e94754eacfdcaf70b067b47203b19
Skstyle007 <shivam001stm@gmail.com>
2026-07-20 14:53:05 +0530

    Scope

---
+++ src/main/java/com/shivam/demo/Scope/LPU.java
@@ -0,0 +1,26 @@
+package com.shivam.demo.Scope;
+
+import com.shivam.demo.studentserver.Student;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component
+@Scope("prototype")
+public class LPU {
+
+    LPU() {
+        System.out.println("LPU rank 1");
+    }
+
+    public void admission(Student student) {
+        System.out.println("admission");
+    }
+
+    public void exam() {
+        System.out.println("exam");
+    }
+
+    public void prepClasses(Student student) {
+        System.out.println("prepClasses");
+    }
+}
